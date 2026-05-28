5d978657bfb4e7cd049a37b114d619e759d27c63
Arush Agrawal <arushagrawal366@gmail.com>
2026-05-28 19:15:11 +0530

    made prediction more accurate

---
+++ upgradedtracker.py
@@ -0,0 +1,33 @@
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+import pandas as pd 
+dataset= pd.read_csv('training.1600000.processed.noemoticon.csv',encoding='latin-1',header=None)
+dataset.columns=["label","userid","time","query","username","tweets"]
+dataset= dataset.sample(100000)
+texts= dataset["tweets"].tolist()
+labels=dataset["label"].replace({4: 1}).tolist()
+# print(texts)
+v= TfidfVectorizer()
+a= v.fit_transform(texts)
+#print(a)
+r_train, r_test, l_train, l_test = train_test_split(a, labels, test_size=0.2)
+m= LogisticRegression()
+m.fit(r_train, l_train)
+
+text= input("enter the text: ")
+
+def prediction(text):
+    v_input= v.transform([text])
+    prediction = m.predict(v_input)[0]
+    probability = m.predict_proba(v_input)[0][prediction]
+    print(probability)
+    return "positive" if prediction == 1 else "negative"
+
+
+result = prediction(text)
+print(result)
+
+
+
